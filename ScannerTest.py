f3dd9f192b3c369068c100d6b86113098dcb0d06
Ryan Unger <unger.ryan5@gmail.com>
2020-11-27 18:37:51 -0600

    Added Sobel filter and adjusted threshold values

--- ScannerTest.py
+++ ScannerTest.py
@@ -1,47 +1,68 @@
 import cv2
 import numpy as np
-import support_functions as sf
+import support_functions
 
-print("Imported Successfully")
-img = cv2.imread("YETIJPG.jpeg")
-heightImg, widthImg, c = img.shape
 
-#sf.initializeTrackbars()
-count = 0
+########################################################################
+webCamFeed = False
+img = cv2.imread("Holly.jpg")
+cap = cv2.VideoCapture(1)
+cap.set(10,160)
+heightImg, widthImg = 1024, 768
+########################################################################
 
-print("Entering while loop \n")
+support_functions.initializeTrackbars()
+count=0
+
+window_name = ('Sobel Demo - Simple Edge Detector')
+scale = 1
+delta = 0
+ddepth = cv2.CV_16S
 
 while True:
-    imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8) # Creates a blank image for testing
-    img = cv2.resize(img, (widthImg, heightImg)) #resize image
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert image to gray scale
-    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) #Add Gaussean Blur
-    thres = sf.valTrackbars() #Get track bar values for thresholds
-    imgThreshold = cv2.Canny(imgBlur, thres[0],thres[1]) #Apply canny blur
-    kernel = np.ones((5,5))
-    imgDial = cv2.dilate(imgThreshold, kernel, iterations = 2) #Apply dilation
-    imgThreshold = cv2.erode(imgDial, kernel, iterations = 1) #Apply erosion
+
+    if webCamFeed:success, img = cap.read()
+    #else:img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
+    imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
+    grad_x = cv2.Sobel(imgBlur, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    # Gradient-Y
+    # grad_y = cv.Scharr(gray,ddepth,0,1)
+    grad_y = cv2.Sobel(imgBlur, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
 
 
-    #Find All Contours
-    imgContours = img.copy()
-    imgBigContour = img.copy()
-    contours, heirarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
 
 
-    #Find the Biggest Contour
-    biggest, maxArea = sf.biggestContour(contours)
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    thres=  75, 125 #support_functions.valTrackbars() # GET TRACK BAR VALUES FOR THRESHOLDS
+    imgThreshold = cv2.Canny(grad,thres[0],thres[1]) # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+
+    ## FIND ALL COUNTOURS
+    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # FIND ALL CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
+
+
+    # FIND THE BIGGEST COUNTOUR
+    biggest, maxArea = support_functions.biggestContour(contours) # FIND THE BIGGEST CONTOUR
     if biggest.size != 0:
-        biggest = sf.reorder(biggest)
-        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) #Draw the biggest contour
-        imgBigContour = sf.drawRectangle(imgBigContour, biggest, 2)
-        pts1 = np.float32(biggest) #Prepare points for warp
-        pts2 = np.float32([[0, 0],[widthImg, 0],[0, heightImg], [widthImg, heightImg]])
+        biggest=support_functions.reorder(biggest)
+        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
+        imgBigContour = support_functions.drawRectangle(imgBigContour,biggest,2)
+        pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+        pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
         imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
-        #Remove 20 Pixels from each side
+        #REMOVE 20 PIXELS FORM EACH SIDE
         imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
         imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
 
@@ -63,7 +84,7 @@ while True:
     lables = [["Original","Gray","Threshold","Contours"],
               ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
 
-    stackedImage = sf.stackImages(imageArray,0.75,lables)
+    stackedImage = support_functions.stackImages(imageArray,0.75,lables)
     cv2.imshow("Result",stackedImage)
 
     # SAVE IMAGE WHEN 's' key is pressed
